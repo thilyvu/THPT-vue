c37d82f1891d976cddbc3596ca84373c7a22bb2e
Thi Ly Vu <vu.thi@nexlab.tech>
2024-05-17 22:45:49 +0700

    change FE base URL

--- FrontEnd/src/api/Base.js
+++ FrontEnd/src/api/Base.js
@@ -2,8 +2,8 @@ import VueCookies from "vue-cookies";
 export let TOKEN = VueCookies.get("token");
 // export const BASE_URL = "https://be-eduhub.herokuapp.com";
 // export const BASE_URL = "https://ieltsbe.tretrau.vn";
-// export const BASE_URL = "https://45.32.57.14:5000";
-export const BASE_URL = "http://localhost:5001";
+export const BASE_URL = "http://14.225.210.27:5001";
+// export const BASE_URL = "http://localhost:5001";
 export const HEADERS = {
   Accept: "application/json",
   "Content-Type": "application/json",
